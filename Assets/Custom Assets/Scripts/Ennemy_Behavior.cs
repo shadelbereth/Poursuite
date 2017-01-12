ff79843437999845196dfd80a14fc631efa896e5
Sophie <sophie.roch@gmail.com>
2017-01-12 16:23:58 +0100

    git project

---
+++ Assets/Custom Assets/Scripts/Ennemy_Behavior.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ennemy_Behavior : MonoBehaviour {
+
+public GameObject Target;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	   GetComponent<NavMeshAgent>().destination = Target.transform.position;
+	}
+
+    void OnTriggerEnter(Collider Other) {
+        print("CRUSHED !!!");
+    }
+}
---
+++ Assets/Custom Assets/Scripts/Vault_Manager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Vault_Manager : MonoBehaviour {
+
+    public int Number;
+
+	// Use this for initialization
+	void Start () {
+	   Number = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other) {
+        Number ++;
+        if (Number > 3) {
+            print("YOU'RE A WINNER AND YOU KNOW IT");
+        }
+    }
+}
